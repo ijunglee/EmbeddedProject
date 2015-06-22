215fa3b75ba584229abe8002360fc8448bd94af1
Michelle <akino_sumiko@hotmail.com>
2015-06-22 12:55:40 +0000

    bluetooth.cpp try to send hello

--- bluetooth.cpp
+++ bluetooth.cpp
@@ -9,8 +9,8 @@
          //char str1[] = "low";
          //char str2[] = "high";
          //char str[] = "60";                       //value to pass to export file
-         char str[] = "BB-UART1"
-         export_file = fopen ("/sys/devices/bone_capemgr.*/slots", "w");
-         fwrite (str, 1, sizeof(str), export_file);
+         char str[] = "Hello";
+         export_file = fopen ("/dev/ttyO1", "w");
+         fwrite (str, sizeof(char), sizeof(str), export_file);
          fclose (export_file);
  }
